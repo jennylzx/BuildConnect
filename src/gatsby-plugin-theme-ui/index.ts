ee2c9fb5013a9f1cd45190ca27603854534cd9cb
Jenny Long <91097644+jennylzx@users.noreply.github.com>
2026-03-14 14:47:05 +0000

    image slow response changed, read me updated

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -5,6 +5,12 @@ const theme = merge(originalTheme, {
   fonts: {
     body: `"Work Sans", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
   },
+  navigation: {
+    a: {
+      fontSize: [1, 2, 2, 3],
+      whiteSpace: `nowrap`,
+    },
+  },
 })
 
 export default theme
